b018e5084443b22c5494c30b01f02a5409e2227d
Duncan Thomas <duncan.thomas@hpe.com>
2025-09-01 23:53:55 +0100

    Buttons are working
    Starting to get the AppStore to work

--- pyscript_main.py
+++ pyscript_main.py
@@ -10,14 +10,28 @@ from js import (
     CanvasRenderingContext2D as Context2d,
     ImageData,
     Uint8ClampedArray,
-    console
+    console,
+    document,
 )
+
+
+async def monkey_patch_http():
+    # requests doesn't work in pyscript without this voodoo
+
+    import micropip
+    await micropip.install("pyodide-http")
+    await micropip.install("requests")
+
+    import pyodide_http
+    pyodide_http.patch_all()
+
 
 def monkey_patch_sys():
     if not hasattr(sys, "print_exception"):
         def print_exception(e, file):
             print("Exception:", e, file=file)
         sys.print_exception = print_exception
+
 
 def monkey_patch_tildagon_helpers():
     class FakeHelpers:
@@ -48,8 +62,8 @@ def monkey_patch_network():
         class FakeWLAN:
             def __init__(self, interface):
                 self.interface = interface
-                self._active = False
-                self._connected = False
+                self._active = True
+                self._connected = True
 
             def active(self, is_active=None):
                 if is_active is None:
@@ -71,7 +85,7 @@ def monkey_patch_network():
             def status(self):
                 if not self._active:
                     return 0
-        
+
         def __init__(self):
             pass
 
@@ -94,19 +108,22 @@ def monkey_patch_time():
     if not hasattr(time, "ticks_add"):
         time.ticks_add = lambda a, b: a + b
 
+
 class FakeCtx:
     def __init__(self):
         self.width = 240
         self.height = 240
-        self.scale = 3 # The number of web pixels per "display" pixel
+        self.scale = 3   # The number of web pixels per "display" pixel
         self.border = 10
+
+        self.font_size = 8
 
         self.CENTER = 1
         self.LEFT = 2
         self.RIGHT = 3
         self.MIDDLE = 4
 
-        self.color = "rgb(0, 255, 0)" # FIXME: find what the default color is
+        self.color = "rgb(0, 255, 0)"   # FIXME: find what the default color is
         self.position = (0, 0)
         self._rectange = None
 
@@ -122,14 +139,21 @@ class FakeCtx:
         ctx.position = self.position
         return ctx
 
+    def gray(self, v):
+        new = self.clone()
+        new.color = f"rgb({v}, {v}, {v})"
+        return new
+
     def save(self):
-        print("Not implemented: FakeCtx: save()")
+#        print("Not implemented: FakeCtx: save()")
+        pass
 
     def restore(self):
-        print("Not implemented: FakeCtx: restore()")
+#        print("Not implemented: FakeCtx: restore()")
+        pass
 
     def move_to(self, x, y):
-        print("ctx.move_to(%s, %s)" % (x, y))
+#        print("ctx.move_to(%s, %s)" % (x, y))
         new = self.clone()
         new.position = (x, y)
         return new
@@ -174,7 +198,11 @@ class FakeCtx:
             ctx.beginPath()
             ctx.arc(
                 (3 * self.width + 2 * self.border) / 2,
-                (3 * self.height + 2 * self.border) / 2,                                        (3 * self.width) / 2,                                                           0,                                                                              2 * math.pi,                                                                )
+                (3 * self.height + 2 * self.border) / 2,
+                (3 * self.width) / 2,
+                0,
+                2 * math.pi,
+            )
             ctx.closePath()
             ctx.clip()
 
@@ -189,7 +217,7 @@ class FakeCtx:
         return self
 
     def text_width(self, text):
-        print("Not properly implemented: FakeCtx: text_width(%s)" % text)
+#        print("Not properly implemented: FakeCtx: text_width(%s)" % text)
         return len(text) * 8
 
     def text(self, text):
@@ -210,7 +238,7 @@ class FakeCtx:
         ctx.fillStyle = self.color
         ctx.font = f"{8 * self.scale}px sans-serif"
         x, y = self.position
-        print("Drawing text at", self._x_to_web(x), self._y_to_web(y), "in color", self.color)
+#        print("Drawing text at", self._x_to_web(x), self._y_to_web(y), "in color", self.color)
         ctx.fillText(text, self._x_to_web(x), self._y_to_web(y))
         ctx.restore()
 
@@ -236,9 +264,11 @@ def monkey_patch_display():
 
         @staticmethod
         def end_frame(ctx):
-            print("Not implemented: FakeDisplay: end_frame()")
+#            print("Not implemented: FakeDisplay: end_frame()")
+            pass
 
     sys.modules["display"] = FakeDisplay
+
 
 def monkey_patch_machine():
     class FakePin:
@@ -275,9 +305,9 @@ def monkey_patch_tildagon():
     class FakePin:
         pass
 
-
     sys.modules["tildagon.ePin"] = FakeEPin
     sys.modules["tildagon.Pin"] = FakePin
+
 
 def monkey_patch_ePin():
     from types import ModuleType
@@ -318,7 +348,7 @@ def monkey_patch_neopixel():
     class FakeNeoPixel:
         def __init__(self, *args, **kwargs):
             self.length = 12
-            self.rgb = [(0,0,0)] * self.length
+            self.rgb = [(0, 0, 0)] * self.length
 
         def write(self):
             for led in range(self.length):
@@ -340,12 +370,12 @@ def monkey_patch_neopixel():
                 print("FakeNeoPixel: Ignoring setitem out of range", item)
             else:
                 self.rgb[item-1] = value
-            
 
     class FakeNeoPixelModule:
         NeoPixel = FakeNeoPixel
 
     sys.modules["neopixel"] = FakeNeoPixelModule
+
 
 async def badge():
     resolution_x = 240
@@ -369,8 +399,8 @@ async def badge():
     ctx = canvas._js.getContext("2d")
 
     # Set the canvas size
-    width= 3 * resolution_x + 2 * border
-    height= 3 * resolution_y + 2 * border
+    width = 3 * resolution_x + 2 * border
+    height = 3 * resolution_y + 2 * border
 
     canvas.style["width"] = f"{width}px"
     canvas.style["height"] = f"{height}px"
@@ -389,7 +419,6 @@ async def badge():
     )
     ctx.fill()
     ctx.closePath()
-
 
     # Draw a black circle for the screen
     ctx.fillStyle = "rgb(0 0 0)"
@@ -410,6 +439,16 @@ async def badge():
     await start_tildagon_os()
 
 
+async def button_handler(event):
+    print("Button pressed:", event.target.id)
+
+    from system.eventbus import eventbus
+    from frontboards.twentyfour import BUTTONS
+    from events.input import ButtonDownEvent, ButtonUpEvent
+    print("Emitting ButtonDownEvent for button", BUTTONS[event.target.id])
+    await eventbus.emit_async(ButtonDownEvent(button=BUTTONS[event.target.id]))
+
+
 async def start_tildagon_os():
     # Fix up differences between MicroPython and PyScript
     monkey_patch_time()
@@ -421,12 +460,13 @@ async def start_tildagon_os():
     monkey_patch_sys()
     monkey_patch_network()
     monkey_patch_tildagon_helpers()
+    await monkey_patch_http()
 
     import main
     # Everything gets started on the import above
+
 
 async def main():
     _ = await asyncio.gather(badge())
 
 asyncio.ensure_future(main())
-
