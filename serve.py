dc10a0204b22a07c94e9542f9167b93ec60da4a1
Jonty Wareing <jonty@jonty.co.uk>
2025-09-04 12:56:00 +0100

    Remove local proxy, CORS workarounds for Github Pages

--- serve.py
+++ serve.py
@@ -1,32 +1,10 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib
-import requests
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 
-class Serv(BaseHTTPRequestHandler):
-    def do_GET(self):
-       if self.path == '/':
-           print("Getting index.html")
-           self.path = '/index.html'
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
 
-       if self.path.startswith("/proxy/"):
-            path = self.path[7:]
-            print("Proxying", path)
-            response = requests.get(path)
-            self.send_response(response.status_code)
-            self.end_headers()
-            self.wfile.write(response.content)
-            return
-       else:
-            try:
-                print("Getting", self.path[1:])
-                file_to_open = open(self.path[1:]).read()
-                self.send_response(200)
-            except:
-                file_to_open = "File not found"
-                self.send_response(404)
-       self.end_headers()
-       self.wfile.write(bytes(file_to_open, 'utf-8'))
-
-httpd = HTTPServer(('localhost',8000), Serv)
-httpd.serve_forever()
-
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer, port=8000)
