395a305cb0503cf466c25050a4d9b2ac95af99a4
Jonty Wareing <jonty@jonty.co.uk>
2026-06-01 00:54:19 +0100

    Use the emulating device IMU for IMU data if possible

---
+++ fakes/imu.py
@@ -0,0 +1,27 @@
+import js
+
+
+def acc_read():
+    """
+    Returns current x, y, z accelerations in m/s**2.
+    """
+    a = js.getAccelerometerData()
+    return (float(a[0]), float(a[1]), float(a[2]))
+
+
+def gyro_read():
+    """
+    Returns current x, y, z rotation rate in degrees/s.
+    """
+    return (0.0, 0.0, 0.0)
+
+
+def pressure_read():
+    """
+    Returns current pressure in Pa and temperature in degree C.
+    """
+    return (7.0, 8.0)
+
+
+def step_counter_read():
+    return 1337
